77ba8df2c269296f19d525a0eacd1090290a7104
Nicolas Oliver <nicolasoliver03@gmail.com>
2017-04-07 14:59:58 -0300

    add force option to ignore isTeamcityContext check

--- index.js
+++ index.js
@@ -13,31 +13,37 @@ function isTeamCityContext() {
 }
 
 function logError(err) {
-  if (!(isTeamCityContext())) { return; }
+  opts = opts || {};
+
+  var shouldReportMessage = opts.force? () => true : isTeamCityContext;
+
+  if (!(shouldReportMessage())) { return; }
   tsm.buildProblem({description: 'Error in plugin \'' + err.plugin + '\' with error: ' + err.message});
 }
 
 function wireTaskEvents(opts) {
-  opts = opts || {}
-  
+  opts = opts || {};
+
+  var shouldReportMessage = opts.force? () => true : isTeamCityContext;
+
   gulp.on('task_start', function (e) {
-    if (!(isTeamCityContext())) { return; }
+    if (!(shouldReportMessage())) { return; }
     tsm.progressStart(e.task);
   });
 
   gulp.on('task_stop', function (e) {
-    if (!(isTeamCityContext())) { return; }
+    if (!(shouldReportMessage())) { return; }
     tsm.progressFinish(e.task);
     opts.sendTaskDuration && tsm.buildStatisticValue({key: 'gulp:' + e.task, value: e.duration * 1000});
   });
 
   gulp.on('task_err', function (e) {
-    if (!(isTeamCityContext())) { return; }
+    if (!(shouldReportMessage())) { return; }
     tsm.buildProblem({description: 'Task \'' + e.task + '\' failed. ' + e.err});
   });
 
   gulp.on('task_not_found', function (e) {
-    if (!(isTeamCityContext())) { return; }
+    if (!(shouldReportMessage())) { return; }
     tsm.buildProblem({description: 'Task \'' + e.task + '\' is not in your gulpfile'});
   });
 }
